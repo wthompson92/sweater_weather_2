2797b01168cb45012859ca4256e35fdf7b9a8a90
username <wnt925@gmail.com>
2019-09-18 13:10:07 -0600

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  @user = User.create(email: 'wnt925@gmail.com', password: "123", password_confirmation: "123", api_key: "fa4b86f9bede0690f34c")
